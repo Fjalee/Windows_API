56abb93c6849956870a4d263d08bbf7497bb0e7b
Rytis Cepulis <cepRytis@gmail.com>
2022-09-12 17:34:30 +0300

    changed button to image, added function to change button color

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@
 
 #define RIBON_HEIGHT 30
 
+#define COLORREF2RGB(Color) (Color & 0xff00) | ((Color >> 16) & 0xff) \
+                                 | ((Color << 16) & 0xff0000)
+
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 
@@ -23,8 +26,8 @@ LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
 
 HMENU hMenu;
-HWND hScore;
-HWND hSpeed;
+HBITMAP hClickPadImage;
+HWND hScore, hSpeed, hClickPad;
 
 int playerScore = 0;
 int playerSpeed = 0;
@@ -95,6 +98,103 @@ void testFilePrint(std::string s)
     std::ofstream MyFile("test.txt");
     MyFile << s;
     MyFile.close();
+}
+
+
+
+//-------------------------------------------------------------------------------
+// hBmp         : Source Bitmap
+// cOldColor : Color to replace in hBmp
+// cNewColor : Color used for replacement
+// hBmpDC    : DC of hBmp ( default NULL ) could be NULL if hBmp is not selected
+//
+// Retcode   : HBITMAP of the modified bitmap or NULL for errors
+//-------------------------------------------------------------------------------
+HBITMAP ReplaceColor(HBITMAP hBmp,COLORREF cOldColor,COLORREF cNewColor,HDC hBmpDC)
+{
+    HBITMAP RetBmp=NULL;
+    if (hBmp)
+    {
+        HDC BufferDC=CreateCompatibleDC(NULL);    // DC for Source Bitmap
+        if (BufferDC)
+        {
+            HBITMAP hTmpBitmap = (HBITMAP) NULL;
+            if (hBmpDC)
+                if (hBmp == (HBITMAP)GetCurrentObject(hBmpDC, OBJ_BITMAP))
+            {
+                hTmpBitmap = CreateBitmap(1, 1, 1, 1, NULL);
+                SelectObject(hBmpDC, hTmpBitmap);
+            }
+
+            HGDIOBJ PreviousBufferObject=SelectObject(BufferDC,hBmp);
+            // here BufferDC contains the bitmap
+
+            HDC DirectDC=CreateCompatibleDC(NULL); // DC for working
+            if (DirectDC)
+            {
+                // Get bitmap size
+                BITMAP bm;
+                GetObject(hBmp, sizeof(bm), &bm);
+
+                // create a BITMAPINFO with minimal initilisation
+                // for the CreateDIBSection
+                BITMAPINFO RGB32BitsBITMAPINFO;
+                ZeroMemory(&RGB32BitsBITMAPINFO,sizeof(BITMAPINFO));
+                RGB32BitsBITMAPINFO.bmiHeader.biSize=sizeof(BITMAPINFOHEADER);
+                RGB32BitsBITMAPINFO.bmiHeader.biWidth=bm.bmWidth;
+                RGB32BitsBITMAPINFO.bmiHeader.biHeight=bm.bmHeight;
+                RGB32BitsBITMAPINFO.bmiHeader.biPlanes=1;
+                RGB32BitsBITMAPINFO.bmiHeader.biBitCount=32;
+
+                // pointer used for direct Bitmap pixels access
+                UINT * ptPixels;
+
+                HBITMAP DirectBitmap = CreateDIBSection(DirectDC,
+                                       (BITMAPINFO *)&RGB32BitsBITMAPINFO,
+                                       DIB_RGB_COLORS,
+                                       (void **)&ptPixels,
+                                       NULL, 0);
+                if (DirectBitmap)
+                {
+                    // here DirectBitmap!=NULL so ptPixels!=NULL no need to test
+                    HGDIOBJ PreviousObject=SelectObject(DirectDC, DirectBitmap);
+                    BitBlt(DirectDC,0,0,
+                                   bm.bmWidth,bm.bmHeight,
+                                   BufferDC,0,0,SRCCOPY);
+
+                       // here the DirectDC contains the bitmap
+
+                    // Convert COLORREF to RGB (Invert RED and BLUE)
+                    cOldColor=COLORREF2RGB(cOldColor);
+                    cNewColor=COLORREF2RGB(cNewColor);
+
+                    // After all the inits we can do the job : Replace Color
+                    for (int i=((bm.bmWidth*bm.bmHeight)-1);i>=0;i--)
+                    {
+                        if (ptPixels[i]==cOldColor) ptPixels[i]=cNewColor;
+                    }
+                    // little clean up
+                    // Don't delete the result of SelectObject because it's
+                    // our modified bitmap (DirectBitmap)
+                       SelectObject(DirectDC,PreviousObject);
+
+                    // finish
+                    RetBmp=DirectBitmap;
+                }
+                // clean up
+                DeleteDC(DirectDC);
+            }
+            if (hTmpBitmap)
+            {
+                SelectObject(hBmpDC, hBmp);
+                DeleteObject(hTmpBitmap);
+            }
+            SelectObject(BufferDC,PreviousBufferObject);
+            // BufferDC is now useless
+            DeleteDC(BufferDC);
+        }
+    }
+    return RetBmp;
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -179,9 +279,17 @@ void ReloadGameRibbon()
 /////////////////////////////////////////////////////////////////////
 ///////////////////////////GameMap///////////////////////////////////
 /////////////////////////////////////////////////////////////////////
+void LoadImages()
+{
+    hClickPadImage = (HBITMAP)LoadImageW(NULL, L".\\clickPad.bmp", IMAGE_BITMAP, 55, 55, LR_LOADFROMFILE);
+}
+
 void AddGameMap(HWND hParentWnd){
     AppendGameStatusRibbon(hParentWnd);
-    CreateWindowW(L"button", L"Test button", WS_VISIBLE | WS_CHILD, 500, 500, 100, 50, hParentWnd, (HMENU)CLICK_PAD_CLICKED, NULL, NULL);
+
+    hClickPadImage = ReplaceColor(hClickPadImage,0x0000ff,0x8900ff, NULL); // replace blue by green
+    HWND hButton = CreateWindowW(L"button", NULL, WS_VISIBLE | WS_CHILD | BS_BITMAP, 500, 500, 50, 50, hParentWnd, (HMENU)CLICK_PAD_CLICKED, NULL, NULL);
+    SendMessageW(hButton, BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hClickPadImage);
 }
 /////////////////////////////////////////////////////////////////////
 ///////////////////////////GameMap///////////////////////////////////
@@ -253,6 +361,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             break;
         case WM_CREATE:
             startTime = GetTickCount();
+            LoadImages();
             AddMenus(hwnd);
             AddGameMap(hwnd);
             break;
